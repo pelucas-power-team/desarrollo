59f993379e31fe71869abef6438288f657d7752b
pandres-95 <51783066+pandres-95@users.noreply.github.com>
2019-06-18 18:49:48 +0200

    El programa para dos jugadores reparte igual que antes pero esta vez todos los metodos estan en clases externas a la clase ventana

--- modulos/prueba_repartir_2j.py
+++ modulos/prueba_repartir_2j.py
@@ -4,83 +4,105 @@ from PyQt5 import QtCore, uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPixmap, QTransform
 
-# Cargar nuestro formulario *.ui
-form_play2 = uic.loadUiType("./interfaces/prueba_triunfo_2j.ui")[0]
+class Cbaraja():
+    def __init__(self):
+        self.__mi_baraja = [0 for i in range(40)]
+        self.__mis_puntos = []
+        self.__puntuables =[[108, 208, 308, 408],[109, 209, 309, 409], [110, 210, 310, 410],[13, 23, 33, 43], [11, 21, 31, 41]]
+        self.__orden =[8, 9]
 
-
-# Crear la Clase MyWindowClass con el formulario cargado.
-class play2(QMainWindow, form_play2):
-    def __init__(self, parent=None):
-        QMainWindow.__init__(self, parent)
-        self.setupUi(self)
-        self.orden = [8, 9]
-        self.mi_carpeta = 'BarajaEsp/'
-        self.formato = '.png'
-        self.mi_baraja = [1 for i in range(4) for j in range(10)]
-
-        self.fdorso(self.formato, self.mi_carpeta)
-
-        self.boton_repartir.clicked.connect(self.frepartir)
-
-    def frepartir(self):
-        carta = [0 for i in range(12)]
-        mi_mano1 = [0 for i in range(6)]
-        mi_mano2 = [0 for i in range(6)]
-        carpeta = self.mi_carpeta
-        formato = self.formato
-
-        self.mi_baraja = self.desordenar()
-
-        for i in range(12):
-            mi_carta = self.mi_baraja[i]
-            carta[i] = carpeta + str(mi_carta) + formato
-            if  i < 6 :
-                mi_mano1[i] = QPixmap(carta[i])
-            else:
-                mi_mano2[i-6] = QPixmap(carta[i])
-
-        self.carta_1.setPixmap(mi_mano1[0])
-        self.carta_2.setPixmap(mi_mano1[1])
-        self.carta_3.setPixmap(mi_mano1[2])
-        self.carta_4.setPixmap(mi_mano1[3])
-        self.carta_5.setPixmap(mi_mano1[4])
-        self.carta_6.setPixmap(mi_mano1[5])
-        self.carta_21.setPixmap(mi_mano2[0])
-        self.carta_22.setPixmap(mi_mano2[1])
-        self.carta_23.setPixmap(mi_mano2[2])
-        self.carta_24.setPixmap(mi_mano2[3])
-        self.carta_25.setPixmap(mi_mano2[4])
-        self.carta_26.setPixmap(mi_mano2[5])
-
-        self.ftriunfo(self.formato, self.mi_carpeta, self.mi_baraja[12])
-
-    def fdorso(self, formato, carpeta):
-
-        mi_dorso = carpeta + '0' + formato
-        mi_imagen_dorso = QPixmap(mi_dorso)
-        self.mazo.setPixmap(mi_imagen_dorso)
-
-    def ftriunfo(self, formato, carpeta, carta):
-        mi_triunfo = carpeta + str(carta) + formato
-        mi_imagen_triunfo = QPixmap(mi_triunfo)
-        mi_imagen_triunfo = mi_imagen_triunfo.transformed(QTransform().rotate(90))
-        self.triunfo.setPixmap(mi_imagen_triunfo)
-
-    def desordenar(self):
+    def __fcrearbaraja(self):
         k = 0
         for i in range(1,5):
             for j in range(1,11):
-                if j not in self.orden:
-                    self.mi_baraja[k] = str(i) + str(j)
+                if j not in self.__orden:
+                    self.__mi_baraja[k] = int(str(i) + str(j),base = 10)
                 else:
-                    self.mi_baraja[k] = str(i) + str(0) + str(j)
-                k += 1
-        r.shuffle(self.mi_baraja)
-        return  self.mi_baraja
+                    self.__mi_baraja[k] = int(str(i) + str(0) + str(j),base = 10)
+                k+=1
+        return(self.__mi_baraja)
+
+    def __fbarajar(self):
+        self.__mi_baraja = self.__fcrearbaraja()
+        r.shuffle(self.__mi_baraja)
+        return(self.__mi_baraja)
+
+    def __fpuntos(self):
+
+        for i in range(len(self.__mi_baraja)):
+            if self.__mi_baraja[i] in self.__puntuables[0]:
+                self.__mis_puntos.append(2)
+            elif self.__mi_baraja[i] in self.__puntuables[1]:
+                self.__mis_puntos.append(3)
+            elif self.__mi_baraja[i] in self.__puntuables[2]:
+                self.__mis_puntos.append(4)
+            elif self.__mi_baraja[i] in self.__puntuables[3]:
+                self.__mis_puntos.append(10)
+            elif self.__mi_baraja[i] in self.__puntuables[4]:
+                self.__mis_puntos.append(11)
+            else:
+                self.__mis_puntos.append(0)
 
 
-"""if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    MyWindow = MyWindowClass(None)
-    MyWindow.show()
-    app.exec_()"""
+        return (self.__mis_puntos)
+
+    def getBaraja(self):
+        baraja = self.__fbarajar()
+        puntos = self.__fpuntos()
+        return (baraja, puntos)
+
+class Cmano(Cbaraja):
+    def __init__(self):
+        super().__init__()
+        self.baraja = []
+        self.puntos = []
+        self.mi_mano = []
+        self.mi_mano_orden = []
+        self.mis_puntos = []
+        self.carta = []
+        self.triunfo = None
+
+
+
+    def frepartir(self,carp, forma,numj):
+        [self.baraja, self.puntos] = self.getBaraja()
+        if numj == 2:
+            for i in range(12):
+                mi_carta = self.baraja[i]
+                self.mi_mano_orden.append(mi_carta)
+                self.mis_puntos.append(self.puntos[i])
+                self.carta.append(carp + str(mi_carta) + forma)
+                self.mi_mano.append(QPixmap(self.carta[i]))
+            self.triunfo = self.baraja[12]
+        elif numj == 4:
+            for i in range(24):
+                mi_carta = self.baraja[i]
+                self.mi_mano_orden.append(mi_carta)
+                self.mis_puntos.append(self.puntos[i])
+                self.carta.append(carp + str(mi_carta) + forma)
+                self.mi_mano.append(QPixmap(self.carta[i]))
+            self.triunfo = self.baraja[12]
+
+        return(self.mi_mano, self.mis_puntos, self.triunfo)
+
+class Ctriunfo():
+    def __init__(self):
+
+        self.mi_triunfo = None
+        self.mi_imagen_triunfo = None
+
+    def ftriunfo(self, carpeta, formato,triunfo ):
+        self.mi_triunfo = carpeta + str(triunfo) + formato
+        self.mi_imagen_triunfo = QPixmap(self.mi_triunfo)
+        self.mi_imagen_triunfo = self.mi_imagen_triunfo.transformed(QTransform().rotate(90))
+        return(self.mi_imagen_triunfo)
+
+class Cdorso():
+    def __init__(self):
+        self.mi_dorso = None
+        self.mi_imagen_dorso = None
+
+    def fdorso(self,carpeta, formato):
+        self.mi_dorso = carpeta +'0' + formato
+        self.mi_imagen_dorso = QPixmap(self.mi_dorso)
+        return(self.mi_imagen_dorso)
--- modulos/ventanas.py
+++ modulos/ventanas.py
@@ -12,11 +12,42 @@ class Window_player_2(QMainWindow, form_class_1):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
-        self.player = play.play2(None)
+        self.carpeta = 'BarajaEsp/'
+        self.formato = '.png'
+        self.mi_mano = []
+        self.mis_puntos = []
+        self.mano = None
+        self.triunfo = None
+        self.mi_triunfo = None
+        self.mazo = None
+        self.boton_repartir.clicked.connect(self.fcargarmano)
+
+    def fcargarmano(self):
+        self.mano = play.Cmano()
+        self.triunfo = play.Ctriunfo()
+        self.mazo = play.Cdorso()
+
+        img_dorso = self.mazo.fdorso(self.carpeta,self.formato)
+        self.i_mazo.setPixmap(img_dorso)
+
+        [self.mi_mano, self.mis_puntos,self.mi_triunfo] = self.mano.frepartir(self.carpeta, self.formato,2)
+        img_triunfo = self.triunfo.ftriunfo(self.carpeta, self.formato, self.mi_triunfo)
+        self.i_triunfo.setPixmap(img_triunfo)
+
+        self.carta_1.setPixmap(self.mi_mano[0])
+        self.carta_2.setPixmap(self.mi_mano[1])
+        self.carta_3.setPixmap(self.mi_mano[2])
+        self.carta_4.setPixmap(self.mi_mano[3])
+        self.carta_5.setPixmap(self.mi_mano[4])
+        self.carta_6.setPixmap(self.mi_mano[5])
+        self.carta_21.setPixmap(img_dorso)
+        self.carta_22.setPixmap(img_dorso)
+        self.carta_23.setPixmap(img_dorso)
+        self.carta_24.setPixmap(img_dorso)
+        self.carta_25.setPixmap(img_dorso)
+        self.carta_26.setPixmap(img_dorso)
 
 
-    def fwindow(self):
-        self.player.show()
 
 
 
