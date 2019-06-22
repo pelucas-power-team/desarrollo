b78acee4039a3c0f1604140415045f695c84b7da
IgnacioFC14 <igfeci2013@gmail.com>
2019-06-22 17:10:12 +0200

    lógica en bruto

--- modulos/logica.py
+++ modulos/logica.py
@@ -3,34 +3,47 @@ from PyQt5 import QtCore, QtGui, uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from modulos import prueba_repartir_2j as play
 
-
 def logica(self):
-    triunfo = list(self.mi_baraja[12])
+    triunfo = list(self.baraja[1][12])#pensando en la baraja matriz, con las 2 denominaciones y la puntuación
     palo_triunfo = triunfo[0]
     valor_triunfo = triunfo[1:2]
     # las dos siguientes hay que ponerlas en función de la carta seleccionada en la interfaz
-    mi_baza_j1 = list() #es el jugador que saca primero
-    mi_baza_j2 = list() #es el jugador que saca segundo
-    if palo_triunfo == mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
-        mostradas = [mi_baza_j1[1:2], mi_baza_j2[1:2]]
-        gana = max(mostradas)
-        ganador = [palo_triunfo, gana]
-    elif palo_triunfo == mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
-        gana = mi_baza_j1[1:2]
-        ganador = [palo_triunfo,gana]
-    elif palo_triunfo != mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
-        gana = mi_baza_j2[1:2]
-        ganador = [palo_triunfo, gana]
-    elif palo_triunfo != mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
-        if mi_baza_j1[0] == mi_baza_j2[0]:
+    mi_baza_j1 = list(baza_j1) #OJO!!!! es el jugador que saca primero
+    mi_baza_j2 = list(baza_j2) #OJO!!!! es el jugador que saca segundo
+    puntuacion_j1=0
+    puntuacion_j2=0
+    puntuacion_j3=0
+    puntuacion_j4=0
+    if numj == 2:
+        if palo_triunfo == mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
             mostradas = [mi_baza_j1[1:2], mi_baza_j2[1:2]]
             gana = max(mostradas)
-            ganador = [mi_baza_j1[0],gana]
-        else
-            ganador = mi_baza_j1
+            ganador = [palo_triunfo, gana]
+            if ganador in mi_mano1:
+                puntuacion_j1=puntuacion_j1+puntos(mi_baza_j1)+puntos(mi_baza_j2)
+            else:
+                puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+        elif palo_triunfo == mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
+            gana = mi_baza_j1[1:2]
+            ganador = [palo_triunfo,gana]
+            puntuacion_j1 = puntuacion_j1 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+        elif palo_triunfo != mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
+            gana = mi_baza_j2[1:2]
+            ganador = [palo_triunfo, gana]
+            puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+        elif palo_triunfo != mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
+            if mi_baza_j1[0] == mi_baza_j2[0]:
+                mostradas = [mi_baza_j1[1:2], mi_baza_j2[1:2]]
+                gana = max(mostradas)
+                ganador = [mi_baza_j1[0],gana]
+                if ganador in mi_mano1:
+                    puntuacion_j1 = puntuacion_j1 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+                else:
+                    puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+            else:
+                ganador = mi_baza_j1 #¿Cómo hacemos para tener en cuenta el jugador que es mano?
 
 
 
     total_jugador1 =
     total_jugador2 =
-
