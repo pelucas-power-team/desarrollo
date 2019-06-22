f4886791bd3675c8c7a4ab798cc599475fe2eca3
IgnacioFC14 <igfeci2013@gmail.com>
2019-06-22 18:50:44 +0200

    lógica en bruto

--- modulos/logica.py
+++ modulos/logica.py
@@ -1,47 +1,47 @@
 import sys
 from PyQt5 import QtCore, QtGui, uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from modulos import prueba_repartir_2j as play
+from modulos import generico as play.Cmano
 
-def logica(self):
-    triunfo = list(self.baraja[1][12])#pensando en la baraja matriz, con las 2 denominaciones y la puntuación
-    palo_triunfo = triunfo[0]
-    valor_triunfo = triunfo[1:2]
-    # las dos siguientes hay que ponerlas en función de la carta seleccionada en la interfaz
-    mi_baza_j1 = list(baza_j1) #OJO!!!! es el jugador que saca primero
-    mi_baza_j2 = list(baza_j2) #OJO!!!! es el jugador que saca segundo
-    puntuacion_j1=0
-    puntuacion_j2=0
-    puntuacion_j3=0
-    puntuacion_j4=0
-    if numj == 2:
-        if palo_triunfo == mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
-            mostradas = [mi_baza_j1[1:2], mi_baza_j2[1:2]]
-            gana = max(mostradas)
-            ganador = [palo_triunfo, gana]
-            if ganador in mi_mano1:
-                puntuacion_j1=puntuacion_j1+puntos(mi_baza_j1)+puntos(mi_baza_j2)
-            else:
-                puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
-        elif palo_triunfo == mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
-            gana = mi_baza_j1[1:2]
-            ganador = [palo_triunfo,gana]
-            puntuacion_j1 = puntuacion_j1 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
-        elif palo_triunfo != mi_baza_j1[0] and palo_triunfo == mi_baza_j2[0]:
-            gana = mi_baza_j2[1:2]
-            ganador = [palo_triunfo, gana]
-            puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
-        elif palo_triunfo != mi_baza_j1[0] and palo_triunfo != mi_baza_j2[0]:
-            if mi_baza_j1[0] == mi_baza_j2[0]:
-                mostradas = [mi_baza_j1[1:2], mi_baza_j2[1:2]]
-                gana = max(mostradas)
-                ganador = [mi_baza_j1[0],gana]
-                if ganador in mi_mano1:
-                    puntuacion_j1 = puntuacion_j1 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
+class Clogica(Cmano):
+    def __init__(self):
+        super().__init__()
+        self.baza = []
+        if numj == 2:
+
+            self.mi_baza, self.mis_puntos = self.fordenar(self.baza, self.dic, self.triunfo)
+            for i in range(len(mi_baza)):
+                for j in range(len(dic[0])):
+                    if mi_baza[i] == dic[j]:
+                        baza_out.append(int(dic[1][j], base=10))
+
+            for i in range(len(ref_global[0])):
+                if ref_global[0][i] == triunfo:
+                    triunfo_carta = int(ref_global[1][i], base=10)
+
+            triunfo_str = list(str(triunfo_carta))
+
+            for i in range(len(baza_out)):
+                aux = baza_out[i]
+                baza_str.append(list(str(aux)))
+
+            if baza_str[0][0] == baza_str[1][0]:
+                gana_puntos = mis_puntos[1]
+                if mi_baza[1] == baza[0]:
+
                 else:
-                    puntuacion_j2 = puntuacion_j2 + puntos(mi_baza_j1) + puntos(mi_baza_j2)
             else:
-                ganador = mi_baza_j1 #¿Cómo hacemos para tener en cuenta el jugador que es mano?
+                if baza_str[0][0] == triunfo_str[0]:
+                    gana_puntos = mis_puntos[0]
+                else:
+                    if baza[0] == mi_baza[0]:
+                        gana_puntos = mis_puntos[0]
+                    else:
+                        gana_puntos = mis_puntos[1]
+
+
+
+
 
 
 
