733729120600be8a48ce783980c03e29969a1a03
pandres-95 <51783066+pandres-95@users.noreply.github.com>
2019-06-25 09:30:06 +0200

    #Pablo, finiquitado

--- prueba_jugadores.py
+++ prueba_jugadores.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5 import QtCore, QtGui, uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 from modulos import ventanas as wind
+from PyQt5.QtGui import QPixmap
 
 
 # Cargar nuestro formulario *.ui
@@ -12,7 +13,14 @@ class MainWindowClass(QMainWindow, main_class):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
-
+        self.fondo_imagen = 'interfaces/fondo.png'
+        self.icono2p = 'interfaces/2PL.PNG'
+        self.icono4p = 'interfaces/4PL.PNG'
+        self.fondo.setPixmap(QPixmap(self.fondo_imagen))
+        self.jugar_2.setIcon(QtGui.QIcon(self.icono2p))
+        self.jugar_2.setIconSize(QtCore.QSize(75,50))
+        self.jugar_4.setIcon(QtGui.QIcon(self.icono4p))
+        self.jugar_4.setIconSize(QtCore.QSize(75, 50))
         self.player_2 = wind.Window_player_2(None)
         #self.player_4 = wind.Window_player_4(None)
 
